3e53ca05cb3e21d5a7e0e4a59bf24f7f95c72f96
AlaaAzzam2311 <105333232+AlaaAzzam2311@users.noreply.github.com>
2023-07-22 03:11:55 +0300

    Update and rename c1 to Portfolio.vue

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="portof">
+    <nav>
+      <div class="logo">LOGO</div>
+      <div class="menu">
+        <router-link style="text-decoration: none; padding-right: 80px" to="/"
+          >Home</router-link
+        >
+        <router-link
+          style="text-decoration: none; padding-right: 80px"
+          to="/about"
+          >About</router-link
+        >
+        <router-link
+          style="text-decoration: none; padding-right: 80px"
+          to="/project"
+          >Project</router-link
+        >
+        <router-link
+          style="text-decoration: none; padding-right: 80px"
+          to="/contact"
+          >Contact</router-link
+        >
+        <router-view />
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Portfolio-view",
+};
+</script>
+
